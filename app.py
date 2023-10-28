42f2909e03c38a94387a83fb2d92f705c0bbc035
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2023-10-28 12:32:28 -0700

    added gunicorn app

--- app.py
+++ app.py
@@ -1,36 +1,7 @@
 from flask import Flask, request
-import datetime
-import os
-from dotenv import load_dotenv
-from twilio.rest import Client
-from .caller import make_call
 from only_calls.main import create_reminder, get_time
-
-
-load_dotenv()
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-client = Client(account_sid, auth_token)
-
-
-def send_message_twilio(from_number, to_number, message_body):
-    try:
-        message = client.messages.create(
-            from_=to_number, body=message_body, to=from_number
-        )
-        print(f"Message sent successfully. SID: {message.sid}")
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-def parse_time(time_string):
-    try:
-        datetime_obj = datetime.datetime.strptime(time_string, "%H:%M:%d:%m:%Y")
-        return datetime_obj
-    except ValueError as e:
-        print(f"Error: {e}")
-        return None
-
+from twilioapp.app import send_message_twilio
+from twilioapp.caller import make_call
 
 app = Flask(__name__)
 
